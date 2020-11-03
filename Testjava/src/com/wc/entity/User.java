3a7e06b945ae7c54cffb90d39d80936d766fa02f
王创创 <64683989+woshiwco@users.noreply.github.com>
2020-11-03 21:04:56 +0800

    添加user实体类

--- Testjava/src/com/wc/entity/User.java
+++ Testjava/src/com/wc/entity/User.java
@@ -1,9 +1,15 @@
 package com.wc.entity;
 
+import java.time.LocalDate;
+
 public class User {
     private String username;
     private String password;
 
     private Integer age;
     private LocalDate bir;
+
+    public String getUsername() {
+        return username;
+    }
 }
