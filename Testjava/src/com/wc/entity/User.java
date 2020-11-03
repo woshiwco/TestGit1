604e7f4962150eba4764bc2ca0d620bad4abf82a
王创创 <64683989+woshiwco@users.noreply.github.com>
2020-11-03 21:06:56 +0800

    Update User.java

--- Testjava/src/com/wc/entity/User.java
+++ Testjava/src/com/wc/entity/User.java
@@ -3,12 +3,13 @@ package com.wc.entity;
 import java.time.LocalDate;
 
 public class User {
+    // 定义姓名
     private String username;
     private String password;
 
     private Integer age;
     private LocalDate bir;
-
+    
     public String getUsername() {
         return username;
     }
